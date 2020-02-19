d296b5982493b54a92220d3717662a6fbd750977
p-the <com.theisen@gmail.com>
2020-02-19 13:24:06 +0100

    refinement structure

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,7 +2,7 @@ import { orders } from "./orders";
 import { quotes } from "./quotes";
 import { combineReducers } from "redux";
 
-export const rootReducer = combineReducers({
+export default combineReducers({
   orders,
   quotes
 });
